f06acb5fa6deea2d3fd1fc1fa5dd1a7aa006dd32
AmanBisht01 <bishtaman1111@gmail.com>
2021-11-14 01:48:53 +0530

    deploy

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors(corsOptions));
-app.use(express.static("public"));
-
 const corsOptions = {
   origin: process.env.ALLOWED_CLIENTS.split(","),
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 };
+app.use(cors(corsOptions));
+app.use(express.static("public"));
+
 const connectDB = require("./config/db");
 connectDB();
 
